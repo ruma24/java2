3cf2309dca061b1915e036552c7810481c6ff873
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-07-25 12:31:27 +0530

    Create Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+class Ex2
+{  
+ public static void main(String[] Strings) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input a value for inch: ");
+        double inch = input.nextDouble();
+        double meters = inch * 0.0254;
+        System.out.println(inch + " inch is " + meters + " meters");
+
+    }
+}
