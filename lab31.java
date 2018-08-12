d40a4d90b873a82f2d27aa8522bf2c322b83509b
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 19:01:03 +0530

    Create lab31.java

---
+++ lab31.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+ class bike2 {
+
+		  int speed=50;  
+
+		}  
+
+		class lab31 extends bike2{  
+
+		  int speed=100;  
+
+		  void display(){  
+
+		   System.out.println(speed);//will print speed of Bike   
+
+		  }  
+
+		  public static void main(String args[]){  
+
+		   lab31 b=new lab31();  
+
+		   b.display();  
+
+		}  
+
+
+
+}
