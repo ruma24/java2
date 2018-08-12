094102817de05a3898e6faa94bd43c16ff25d1bd
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:27:39 +0530

    Create lab11.java

---
+++ lab11.java
@@ -0,0 +1,41 @@
+package lab11;
+
+
+
+/**
+
+ *
+
+
+
+ */
+
+public class Lab11 {
+
+
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+    Lab11()
+
+    {
+
+        System.out.println("HI IT");
+
+    }
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+        new Lab11();
+
+    }
+
+    
+
+}
