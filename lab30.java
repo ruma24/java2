98628b4b4866d4d74debc74da9373d2caf9ea355
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:59:15 +0530

    Create lab30.java

---
+++ lab30.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+ class bike1 {
+
+		void run(){System.out.println("Vehicle is running");}  
+
+		}  
+
+		public class lab30 extends bike1{  
+
+			bike1 b=new bike1();
+
+		void run(){System.out.println("Bike is running safely");
+
+		}  
+
+		  
+
+		public static void main(String args[]){  
+
+		lab30 obj = new lab30();  
+
+		obj.run();  
+
+		}  
+
+
+
+}
