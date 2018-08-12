b3664971d2f19243e2ee9b3deb126b8ad4adf5e4
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:06:31 +0530

    Create lab2.java

---
+++ lab2.java
@@ -0,0 +1,49 @@
+package lab2;
+
+
+
+/**
+
+ *
+
+ * @author Karthikeyan
+
+ */
+
+public class Lab2 {
+
+
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+   
+
+        // TODO code application logic here 
+
+	 int id=10;//data member (also instance variable)  
+
+	 String name="karthi";//data member(also instance variable)  
+
+  
+
+	 public static void main(String args[]){  
+
+	  Lab2 s1=new Lab2();//creating an object of Student  
+
+	  System.out.println(s1.id);  
+
+	  System.out.println(s1.name);  
+
+ } 
+
+        }  
+
+
+
+
+
+    
