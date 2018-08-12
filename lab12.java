b19caf316ae2a42b0e3a2797937d650e6592f3e7
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:28:55 +0530

    Create lab12.java

---
+++ lab12.java
@@ -0,0 +1,49 @@
+package lab12;
+
+
+
+/**
+
+ *
+
+ 
+
+ */
+
+public class Lab12 {
+
+
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+    int rno;
+
+    String name;
+
+    Lab12()
+
+    {
+
+        System.out.println(rno+" "+name);
+
+    }
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+        Lab12 l=new Lab12();
+
+        l.rno=25;
+
+        l.name="karthi";
+
+    }
+
+    
+
+}
