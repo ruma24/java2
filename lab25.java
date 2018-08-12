4bf7aaaca58726a55f043046e1ad194b7262b563
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:50:24 +0530

    Create lab25.java

---
+++ lab25.java
@@ -0,0 +1,26 @@
+class lab2 {
+
+		lab2 getA(){  
+
+			return this;  
+
+			}  
+
+			void msg(){System.out.println("Hello java");}  
+
+			}  
+
+			  
+
+			public class lab25{  
+
+			public static void main(String args[]){  
+
+			new lab2().getA().msg();  
+
+			}  
+
+			}  
+
+
+
