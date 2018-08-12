1431aa1270d3d553ce49c17a9036e511af0f81fd
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 20:21:32 +0530

    Create lab33.java

---
+++ lab33.java
@@ -0,0 +1,39 @@
+class bike4 {
+
+	//int speed=50;
+
+	 bike4()
+
+	 {
+
+		 System.out.println("Vehicle is created");
+
+	 }
+
+}  
+
+public class lab33 extends bike4{  
+
+  //int speed=100;  
+
+  lab33(){
+
+	  super();
+
+   System.out.println("The vehicle which is created is BIKE");//will print speed of Bike   
+
+  }  
+
+  public static void main(String args[]){  
+
+   lab33 b=new lab33();  
+
+   
+
+}  
+
+
+
+
+
+}
