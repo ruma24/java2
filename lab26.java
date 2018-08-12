030d247609b7a6b604ae281fd58f08efe0309e9d
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:53:37 +0530

    Create lab26.java

---
+++ lab26.java
@@ -0,0 +1,21 @@
+ class lab1 {
+
+	float salary=40000;  
+
+		}  
+
+		public class lab26 extends lab1{  
+
+		 int bonus=10000;  
+
+	 public static void main(String args[]){  
+
+		   lab26 p=new lab26();  
+
+		   System.out.println("Programmer salary is:"+p.salary);  
+
+		   System.out.println("Bonus of Programmer is:"+p.bonus);  
+
+		}  
+
+}
