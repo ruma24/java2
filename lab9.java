f20eaac4a5029df37e538039a92345d326e9f7e8
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-12 18:23:47 +0530

    Create lab9.java

---
+++ lab9.java
@@ -0,0 +1,45 @@
+package lab9;
+
+
+
+/**
+
+ *
+
+ 
+
+ */
+
+public class Lab9 {
+
+
+
+    /**
+
+     * @param args the command line arguments
+
+     */
+
+      public static void main(int a)
+
+        {
+
+            System.out.println(a);
+
+        }
+
+    public static void main(String[] args) {
+
+        // TODO code application logic here
+
+      
+
+        main(5);
+
+        
+
+    }
+
+    
+
+}
