b10b65af5da11c7aa62ad396ec090ad06bc4897c
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:30:12 +0530

    Create lab74.java

---
+++ lab74.java
@@ -0,0 +1,15 @@
+public class lab74 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="ms";
+
+		String s2="DHONI";
+
+		System.out.println(s1.toUpperCase()+" "+s2.toLowerCase());
+
+	}
+
+}
