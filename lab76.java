fa5f0b3fb9aad6d1ed34213a2dcbfe3c8dcd21c5
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:31:53 +0530

    Create lab76.java

---
+++ lab76.java
@@ -0,0 +1,21 @@
+public class lab76 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="ms";
+
+		String s2="DHONI";
+
+		String s3=s1+s2;
+
+		System.out.println(s3.startsWith("ms"));
+
+		System.out.println(s3.endsWith("i"));
+
+	}
+
+
+
+}
