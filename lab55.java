f1324676da21f05fadd600064ccb8868bc90fa4f
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:12:27 +0530

    Create lab55.java

---
+++ lab55.java
@@ -0,0 +1,21 @@
+package pack1;
+
+
+
+public class lab55 {
+
+	public static void main(String ar[])
+
+	{
+
+		lab_55 b=new lab_55();
+
+		b.setname("karthi");
+
+		System.out.println("The name is "+b.getname());
+
+	}
+
+
+
+}
