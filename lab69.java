1bba5094a7855d30e88cf21031accca14cc538cd
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:26:03 +0530

    Create lab69.java

---
+++ lab69.java
@@ -0,0 +1,19 @@
+public class lab69 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="MS";
+
+		String s2="MS";
+
+		String s3="Dhoni";
+
+		System.out.println(s1==s2);
+
+		System.out.println(s1==s3);
+
+	}
+
+}
