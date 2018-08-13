02ce4dbaf83ab90108f7a7fe4ced0350119c372e
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 09:03:59 +0530

    Create lab98.java

---
+++ lab98.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class lab98 {
+
+public static void main(String ar[])
+
+{
+
+	StringTokenizer st=new StringTokenizer("HI Java");
+
+	while(st.hasMoreTokens())
+
+	{
+
+		System.out.println(st.nextToken());
+
+	}
+
+}
+
+}
