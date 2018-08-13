32c91f7b0250cbc278cd6843b6845a405cff14c4
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:29:29 +0530

    Create lab73.java

---
+++ lab73.java
@@ -0,0 +1,21 @@
+public class lab73 {
+
+	public static void main(String ar[])
+
+	{
+
+		String cap="MS";
+
+		String name=cap.concat(" Dhoni");
+
+		System.out.println(name.substring(3));
+
+		System.out.println(name.substring(0,4));
+
+		
+
+	}
+
+
+
+}
