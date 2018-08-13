9030bf3a912089138d7ac53720fae02ad5ca8052
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:14:26 +0530

    Create lab57.java

---
+++ lab57.java
@@ -0,0 +1,37 @@
+public class lab57 {
+
+	public static void main(String ar[])
+
+	{
+
+int i;
+
+char[] a=new char[6];
+
+a[0]='k';
+
+a[1]='a';
+
+a[2]='r';
+
+a[3]='t';
+
+a[4]='h';
+
+a[5]='i';
+
+for(i=0;i<6;i++)
+
+{
+
+	System.out.println(a[i]);
+
+}
+
+
+
+
+
+}
+
+}
