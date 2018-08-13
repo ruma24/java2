212fca7037ec296acaf56b3a82f1832c2f681c8f
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:32:41 +0530

    Create lab77.java

---
+++ lab77.java
@@ -0,0 +1,17 @@
+public class lab77 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="ms";
+
+		String s2="DHONI";
+
+		System.out.println(s1.charAt(1)+" "+s2.charAt(3));
+
+	}
+
+
+
+}
