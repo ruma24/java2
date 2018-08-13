9855e73aa5b624e1a0506007c613bd73c98ec578
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:26:57 +0530

    Create lab70.java

---
+++ lab70.java
@@ -0,0 +1,21 @@
+public class lab70 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="MS";
+
+		String s2="MS";
+
+		String s3="Dhoni";
+
+		System.out.println(s1.compareTo(s2));
+
+		System.out.println(s1.compareTo(s3));
+
+	}
+
+
+
+}
