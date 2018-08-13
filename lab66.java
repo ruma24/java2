66c44672c980b12af152f6e553503c78095ed015
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:23:31 +0530

    Create lab66.java

---
+++ lab66.java
@@ -0,0 +1,19 @@
+public class lab66 {
+
+	public static void main(String ar[])
+
+	{
+
+		String j="Java";
+
+		char ch[]={'k','a','r','t','h','i'};
+
+		String j1=new String(ch);
+
+		System.out.println(j+" "+j1);
+
+	}
+
+
+
+}
