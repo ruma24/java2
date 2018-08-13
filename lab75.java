e44fcc8c9579117767ed41aec04e2018b849dbed
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:30:56 +0530

    Create lab75.java

---
+++ lab75.java
@@ -0,0 +1,17 @@
+public class lab75 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="ms";
+
+		String s2=" DHONI";
+
+		String s3=s1+s2;
+
+		System.out.println(s2.trim());
+
+	}
+
+}
