ca353ec6c72e8c763a54ce3eb92d1c23bcde76d1
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:59:13 +0530

    Create lab93.java

---
+++ lab93.java
@@ -0,0 +1,15 @@
+public class lab93 {
+
+	public static void main(String ar[])
+
+	{
+
+		StringBuffer s1=new StringBuffer("Information ");
+
+	     s1.reverse();
+
+		System.out.println(s1);
+
+	}
+
+}
