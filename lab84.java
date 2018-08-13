08bb5d0c892712f951a23ac9ace01aff3385a63c
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:38:53 +0530

    Create lab84.java

---
+++ lab84.java
@@ -0,0 +1,17 @@
+public class lab84 {
+
+	public static void main(String ar[])
+
+	{
+
+		StringBuffer s1=new StringBuffer("Information ");
+
+	     s1.replace(1,3,"Technology");
+
+		System.out.println(s1);
+
+	}
+
+
+
+}
