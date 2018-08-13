8869cea5b005bb11288db2f517ae152d00330c4d
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 09:12:50 +0530

    Create lab116.java

---
+++ lab116.java
@@ -0,0 +1,23 @@
+public class lab116 extends Thread{
+
+	public void run()
+
+	{
+
+		System.out.println("Thread is running");
+
+	}
+
+	public static void main(String sr[])
+
+	{
+
+		lab116 g=new lab116();
+
+		g.run();
+
+	}
+
+
+
+}
