f959c0bcfe73741f299c817f963171917181369b
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:27:49 +0530

    Create lab71.java

---
+++ lab71.java
@@ -0,0 +1,19 @@
+public class lab71 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s1="MS";
+
+		String s2=" Dhoni";
+
+		String s3=s1+s2;
+
+		System.out.println(" " +s3);
+
+	}
+
+
+
+}
