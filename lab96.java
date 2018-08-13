4a06c519faf3bc9cf86f354f00d74628817ee8d5
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 09:02:44 +0530

    Create lab96.java

---
+++ lab96.java
@@ -0,0 +1,33 @@
+class stud
+
+{
+
+	int rno;
+
+	String name;
+
+	stud(int rno,String name)
+
+	{
+
+		this.rno=rno;
+
+		this.name=name;
+
+	}
+
+}
+
+public class lab96 {
+
+public static void main(String ar[])
+
+{
+
+	stud s=new stud(25,"karthi");
+
+	System.out.println(s);
+
+}
+
+}
