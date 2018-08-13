cd586492d269552ca7858e0efcc40a3b2aa37468
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:44:25 +0530

    Create lab90.java

---
+++ lab90.java
@@ -0,0 +1,15 @@
+public class lab90 {
+
+	public static void main(String ar[])
+
+	{
+
+		StringBuffer s1=new StringBuffer("Information ");
+
+	     s1.insert(1,"Technology");
+
+		System.out.println(s1);
+
+	}
+
+}
