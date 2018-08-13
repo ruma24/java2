5003b5da34a845092eecdd28eaa3e51a57220c55
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:35:13 +0530

    Create lab80.java

---
+++ lab80.java
@@ -0,0 +1,17 @@
+public class lab80 {
+
+	public static void main(String ar[])
+
+	{
+
+		int a=10;
+
+		String s1=String.valueOf(a);
+
+		//String s2=s1.intern();
+
+		System.out.println(s1+10);
+
+	}
+
+}
