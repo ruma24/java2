8775b55601c1e3b6bbf928556fe6c4e47f4fa74e
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 07:42:57 +0530

    Create lab41.java

---
+++ lab41.java
@@ -0,0 +1,21 @@
+public class lab41 {
+
+private void get()
+
+{
+
+	System.out.println("Running.....");
+
+}
+
+public static void main(String ar[])
+
+{
+
+	lab41 l=new lab41();
+
+	l.get();
+
+}
+
+}
