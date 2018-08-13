66f2b93faf380df98e76bd9ce4e9e31be255e721
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:36:05 +0530

    Create lab81.java

---
+++ lab81.java
@@ -0,0 +1,19 @@
+public class lab81 {
+
+	public static void main(String ar[])
+
+	{
+
+		String s="Oak is a Object Oriented porgramming Language.Java is my favourite language";
+
+		//String s2=s1.intern();
+
+		String Replace=s.replaceAll("Oak","Java");
+
+		System.out.println("Before Changing:"+s);
+
+		System.out.println(Replace);
+
+	}
+
+}
