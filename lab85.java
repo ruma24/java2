bdfae03be0f8de45b6063b78460544ac4af4ddfd
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:39:56 +0530

    Create lab85.java

---
+++ lab85.java
@@ -0,0 +1,15 @@
+public class lab85 {
+
+	public static void main(String ar[])
+
+	{
+
+		StringBuffer s1=new StringBuffer("Information ");
+
+	     s1.delete(1,3);
+
+		System.out.println(s1);
+
+	}
+
+}
