0a47d1c87934e0c64b4791d2e629af4bc4fe5200
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 07:53:30 +0530

    Create lab51.java

---
+++ lab51.java
@@ -0,0 +1,33 @@
+class c1
+
+{
+
+	private int data=10;
+
+	private void dat()
+
+	{
+
+		System.out.println("hi");
+
+	}
+
+}
+
+public class lab51 {
+
+	public static void main(String ar[])
+
+	{
+
+		c1 s=new c1();
+
+		int c=s.data;
+
+		s.dat();
+
+	}
+
+
+
+}
