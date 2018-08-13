ce8ba5968936fa51bb49481ca4f498e03cd2ba4e
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 07:44:02 +0530

    Create lab42.java

---
+++ lab42.java
@@ -0,0 +1,30 @@
+class lab0 {
+
+	 void get()
+
+	{
+
+		System.out.println("Running.....");
+
+	}
+
+}
+
+public class lab42 extends lab0
+
+{
+
+	public static void main(String ar[])
+
+	{
+
+		lab42 l=new lab42();
+
+		l.get();
+
+	}
+
+
+
+}
+
