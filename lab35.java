d726aeb1852f36d92d6fa6e4abd976bd18c81d21
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 07:35:30 +0530

    Create lab35.java

---
+++ lab35.java
@@ -0,0 +1,23 @@
+public class lab35 {
+
+	final int speed=50;
+
+	void run()
+
+	{
+
+		speed=200;
+
+	}
+
+	
+
+  public static void main(String args[]){  
+
+   lab35 b=new lab35();  
+
+   b.run();  
+
+}  
+
+}
