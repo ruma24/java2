7e8177db31f87a842aaf2befcdef973fc868a2cc
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:11:39 +0530

    Create lab54.java

---
+++ lab54.java
@@ -0,0 +1,27 @@
+class c2
+
+{
+
+	public void cc()
+
+	{
+
+		System.out.println("Karthikeyan");
+
+	}
+
+}
+
+public class lab54 extends c2{
+
+public static void main(String ar[])
+
+{
+
+	lab54 b=new lab54();
+
+	b.cc();
+
+}
+
+}
