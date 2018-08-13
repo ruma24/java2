7a499075c2897d574c4c49d76f64cd052d0c955d
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 07:38:47 +0530

    Create lab38.java

---
+++ lab38.java
@@ -0,0 +1,25 @@
+ class bike8 {
+
+	//int speed=50;  
+
+	final void display(){  
+
+		   System.out.println("IT Department");   
+
+		  }  
+
+}  
+
+class lab38 extends bike8
+
+{   
+
+  public static void main(String args[]){  
+
+   lab38 b=new lab38();  
+
+   b.display();  
+
+}  
+
+}
