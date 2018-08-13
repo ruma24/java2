77105f0cdea7f4280b0675f0ae6140acb4d76785
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:19:37 +0530

    Create lab62.java

---
+++ lab62.java
@@ -0,0 +1,21 @@
+public class lab62 {
+
+	public static void main(String ar[])
+
+	{
+
+		int a=10;
+
+		Integer i=Integer.valueOf(a);
+
+		Integer j=i;
+
+		System.out.println(a+" "+i+" "+j);
+
+		
+
+	}
+
+
+
+}
