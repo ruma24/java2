ffed154cade6582e8379f1da2f507dafc9d50816
ruma24 <40590446+ruma24@users.noreply.github.com>
2018-08-13 08:36:56 +0530

    Create lab82.java

---
+++ lab82.java
@@ -0,0 +1,15 @@
+public class lab82 {
+
+	public static void main(String ar[])
+
+	{
+
+		StringBuffer s1=new StringBuffer("Information ");
+
+	     s1.append("Technology");
+
+		System.out.println(s1);
+
+	}
+
+}
